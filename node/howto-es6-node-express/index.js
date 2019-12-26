02635c8c3b7cc73c711e67f9e63c919b5d088740
jnhustin <jnhustin@gmail.com>
2019-12-26 14:00:00 -0800

    initial stuff

---
+++ node/howto-es6-node-express/index.js
@@ -0,0 +1,25 @@
+// import express from 'express'
+// import bodyParser from 'body-parser'
+const express    =  require('express');
+const bodyParser =  require('body-parser');
+
+const print =  console.log;
+const app   =  express();
+const PORT  =  process.env.PORT || 3000;
+
+// settings
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+// modules
+app.get('/', (req, res) => {
+  print('hello from /');
+  res.send('new response');
+});
+
+// listener
+app.listen(PORT, function() {
+  //print(`listening on port ${PORT}`);
+  print('listening on port:',  PORT);
+});
+
