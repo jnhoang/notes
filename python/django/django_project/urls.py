9c24ada739942c320730eb375a331a59c26fadf1
jnhustin <jnhustin@gmail.com>
2020-04-12 12:19:24 -0700

    lesson 8 - user profiles

--- python/django/django_project/urls.py
+++ python/django/django_project/urls.py
@@ -17,6 +17,10 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from users import views as user_views
+from django.conf import settings
+from django.conf.urls.static import static
+
+
 
 urlpatterns = [
   path('admin/', admin.site.urls),
@@ -24,8 +28,14 @@ urlpatterns = [
   path('profile/', user_views.profile, name='profile'),
   path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
   path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
-  path('', include('blog.urls')),                                     # see notes on empty-path
+  path('', include('blog.urls')),           # see notes on empty-path
 ]
+
+if settings.DEBUG:
+  urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+
 
 
 """ NOTES
