2271de972d55532455559253e84a2016d1f7197d
jnhustin <jnhustin@gmail.com>
2020-04-17 15:21:50 -0700

    lesson 9 - profile saves

--- python/django/users/forms.py
+++ python/django/users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
 class UserRegisterForm(UserCreationForm):
   email = forms.EmailField(required=True)       # see notes on required
@@ -19,3 +20,15 @@ class UserRegisterForm(UserCreationForm):
     - what fields to show on the form
     - what order to show the fields in
  """
+
+
+class UserUpdateForm(forms.ModelForm):
+  class Meta:
+    model  =  User
+    fields =  ['username', 'email']
+
+
+class ProfileUpdateForm(forms.ModelForm):
+  class Meta:
+    model  =  Profile
+    fields =  ['image']
--- python/django/users/views.py
+++ python/django/users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages     # see notes.message
 from django.contrib.auth.decorators import login_required
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
 
 # Create your views here.
@@ -21,7 +21,24 @@ def register(request):
 
 @login_required
 def profile(request):
-  return render(request, 'users/profile.html')
+  if request.method == 'POST':
+    u_form = UserUpdateForm(request.POST, instance=request.user)  # see notes on instance
+    p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+
+    if u_form.is_valid() and p_form.is_valid():
+      u_form.save()
+      p_form.save()
+      messages.success(request, f'Your account has been updated!')
+      return redirect('profile')
+  else:
+    u_form = UserUpdateForm(instance=request.user)
+    p_form = ProfileUpdateForm(instance=request.user.profile)
+
+  context = {
+    'u_form': u_form,
+    'p_form': p_form,
+  }
+  return render(request, 'users/profile.html', context)
 
 
 
@@ -29,4 +46,7 @@ def profile(request):
   messages
     - sends flash messages (one time message)
     - can be themed (like bootstrap values) [debug, info, success, warning, error]
+  instance
+    - takes a param of the given class (user/profile)
+    - request has an instance of the current (logged in user)
  """
