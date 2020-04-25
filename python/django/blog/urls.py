d94e63973c5867874d283c678e403b572fd63076
jnhustin <jnhustin@gmail.com>
2020-04-25 10:59:55 -0700

    lesson 11 - pagination

--- python/django/blog/urls.py
+++ python/django/blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import (
   PostListView,
+  UserPostListView,
   PostDetailView,
   PostCreateView,
   PostUpdateView,
@@ -11,6 +12,7 @@ from . import views # (. == current directory)
 urlpatterns = [
   # path('', views.home, name='blog-home'), # see note on blog-home
   path('', PostListView.as_view(), name='blog-home'),     # see notes on clas-based-views
+  path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
   path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
   path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
   path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
